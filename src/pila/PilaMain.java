fd0ee0353ea5ef41812ef78c4a43778f020b2811
reposEntornos <122789051+reposEntornos@users.noreply.github.com>
2024-04-14 11:58:28 +0200

    Update PilaMain.java
    Añadidos comentarios

--- src/pila/PilaMain.java
+++ src/pila/PilaMain.java
@@ -3,8 +3,10 @@ package pila;
 public class PilaMain {
 
 	public static void main(String[] args) {
-		
+		// Crear un objeto Pila
 		Pila p = new Pila();
+
+		// Mostramos la cima de la pila antes de insertar, nos dará null porque está vacía
 		System.out.println("Cima:"+ p.cima());
 		for (int i=1; i<=5; i++) {
 			p.apilar(i);
